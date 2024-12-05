339ae1ee8bffd293f4a56e2ea2948a892312bff2
cpbennett01 <bennettc4@vcu.edu>
2024-12-05 05:16:00 -0500

    createindex.html

---
+++ main.js
@@ -0,0 +1,27 @@
+$( "#maildraggable" ).draggable();
+
+$( "#crowdraggable" ).draggable();
+
+$( "#barkdraggable" ).draggable();
+
+$( "#keydraggable" ).draggable();
+
+$( "#huskdraggable" ).draggable();
+
+$( "#moss1draggable" ).draggable();
+
+$( "#shelldraggable" ).draggable();
+
+$( "#moss2draggable" ).draggable();
+
+$( "#sticksdraggable" ).draggable();
+
+$( "#twig1draggable" ).draggable();
+
+$( "#twig2draggable" ).draggable();
+
+$( "#clipdraggable" ).draggable();
+
+$( "#twig3draggable" ).draggable();
+
+$( "#pindraggable" ).draggable();
